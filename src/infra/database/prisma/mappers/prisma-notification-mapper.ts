b5c4b6c0d542476aa1487c82420c1700fcc6aa13
devjohn42 <devjohncb@gmail.com>
2025-01-28 19:48:57 -0300

    implementing prisma repository (existing entities ID | reverse path in mapper)

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
+import { Content } from '@app/entities/content'
 import { Notification } from '@app/entities/notification'
+import { Notification as RawNotification } from '@prisma/client'
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -10,5 +12,19 @@ export class PrismaNotificationMapper {
       createdAt: notification.createdAt,
       readAt: notification.readAt,
     }
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        recipientId: raw.recipientId,
+        category: raw.category,
+        content: new Content(raw.content),
+        createdAt: raw.createdAt,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+      },
+      raw.id,
+    )
   }
 }
